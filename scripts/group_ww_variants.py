c0e4cf11fe194df33d41654a6805b01644cf0f05
DOH-SML1303 <stephanie.lundert@gmail.com>
2023-05-22 17:58:06 +0000

    updated code

--- scripts/group_ww_variants.py
+++ scripts/group_ww_variants.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 ww_df = pd.read_csv('~/wastewater-surveillance/data/WW_master.csv')
-variants = pd.read_csv('~/wastewater-surveillance/data/variants.csv')
-
 groupby_variants = ww_df.groupby('Variant_name')['Variant_name'].apply(list)
 groupby_variants.to_csv('~/wastewater-surveillance/data/groupby_variants.csv')
-grouped_variants = pd.read_csv('~/wastewater-surveillance/data/groupby_variants.csv')
 
